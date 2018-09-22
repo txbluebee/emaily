b68d7f4b87bd60efd77fc7922fe1a6cb5c2b299c
Brian Fan <kuomu.fan@yahoo.com>
2018-09-21 21:41:14 -0700

    change greeting

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ dog: "Lily Girl" });
 });
 
 const PORT = process.env.PORT || 5000;
-// So what this says is if there is an environment variable that has been already defined by Heroku go
-// ahead and sign that variable to port otherwise by default just use the value of 5000.
-// And so in our development environment we'll use 5000.
 app.listen(PORT);
